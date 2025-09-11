008df0f2370143aa4b432089eecb979813724c4d
Jonathan Green <jonathan@razzard.com>
2025-09-11 13:21:02 -0300

    Update docstring for download_feed function

--- src/palace_tools/cli/download_feed.py
+++ src/palace_tools/cli/download_feed.py
@@ -100,7 +100,7 @@ def download_overdrive_url(
     ),
     url: str = typer.Argument(..., help="URL to fetch"),
 ) -> None:
-    """Output single Overdrive feed URL (metadata, availability, etc)."""
+    """Output Overdrive feed data by URL (metadata, availability, etc)."""
     results = asyncio.run(overdrive.fetch_url(client_key, client_secret, url))
 
     print(json.dumps(results, indent=4))
