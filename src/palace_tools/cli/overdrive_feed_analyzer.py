b14cdd299e7957f90afbd603c7027bdfcc562cad
Daniel Bernstein <dabylon@gmail.com>
2025-10-03 15:05:50 -0700

    Add overdrive_feed_analyzer tool which contains two sub-tools: stats and list-identifiers.

---
+++ src/palace_tools/cli/overdrive_feed_analyzer.py
@@ -0,0 +1,168 @@
+import json
+from pathlib import Path
+from typing import Any, Set
+
+import typer
+
+from palace_tools.feeds.axis import availability
+from palace_tools.utils.typer import run_typer_app_as_main
+
+COPIES_OWNED = "copies owned"
+
+COPIES_AVAILABLE = "copies available"
+
+COPIES_SHARED = "owned copies shared"
+COPIES_UNSHARED = "owned copies unshared"
+
+app = typer.Typer()
+
+
+@app.command("stats")
+def stats (
+    input_file: Path = typer.Argument(
+        ..., help="Input json file (overdrive feed)",  file_okay=True, dir_okay=False
+    ),
+
+) -> None:
+    """
+    This command outputs a json map with some file stats.
+    """
+    accounts_stats: dict(Any, Any) = {}
+    stats: dict(Any, Any) = {}
+
+    with input_file.open("r") as input:
+        data = json.load(input)
+        titles: dict[str, Any] = {}
+        stats["titles"] =  titles
+
+        for datum in data:
+            overdrive_id = datum["id"]
+            title_info: dict[str, Any] = titles.get(overdrive_id, {})
+            titles[overdrive_id] = title_info
+            title_info["title"] = datum["title"]
+            availability = datum.get("availabilityV2", None)
+            availability_array = titles.get("availabilityV2", [])
+            if not availability:
+                print(f"no availabilityV2 found where book id = {datum['id']}\n")
+            else:
+                title_info["availability"] = availability_array
+                availability_array.append(availability)
+                accounts = availability["accounts"]
+                book_level_copies_owned = availability.get("copiesOwned", 0)
+                book_level_copies_available = availability.get("copiesAvailable", 0)
+                stats["feed-wide copies owned"] = stats.get("feed-wide copies owned",0) + book_level_copies_owned
+                stats["feed-wide copies available"] = stats.get("feed-wide copies available",0) + book_level_copies_available
+                for account in accounts:
+                    account_id = str(account["id"])
+                    shared = account.get("shared", False)
+                    copies_available = account.get("copiesAvailable", 0)
+                    copies_owned = account.get("copiesOwned", 0)
+                    account_stats = accounts_stats.get(account_id, {})
+                    accounts_stats[account_id] = account_stats
+                    if shared:
+                        account_stats[COPIES_SHARED] = copies_owned + account_stats.get(COPIES_SHARED, 0)
+                        account_stats[COPIES_UNSHARED] = account_stats.get(COPIES_UNSHARED, 0)
+                    else:
+                        account_stats[COPIES_UNSHARED] = copies_owned + account_stats.get(COPIES_UNSHARED, 0)
+                        account_stats[COPIES_SHARED] = account_stats.get(COPIES_SHARED, 0)
+
+                    account_stats[COPIES_AVAILABLE] = copies_available + account_stats.get(COPIES_AVAILABLE, 0)
+                    account_stats[COPIES_OWNED] = copies_owned + account_stats.get(COPIES_OWNED, 0)
+
+
+
+        stats["file_name"] = input.name
+        stats["unique titles"] = len(titles)
+        stats["title entries in file"] = len(data)
+
+
+    accounts_stats["total advantage plus copies shared"] = sum([x[COPIES_SHARED] for x in accounts_stats.values()])
+    stats["accounts"] = accounts_stats
+
+    print(json.dumps(stats, sort_keys=True, indent=4))
+
+
+@app.command("list-identifiers")
+def list_identifiers (
+    input_file: Path = typer.Argument(
+        ..., help="Input json file (overdrive feed)",  file_okay=True, dir_okay=False
+    ),
+    output_file: Path = typer.Argument(
+        ..., help="Output file", writable=True, file_okay=True, dir_okay=False
+    ),
+
+    filter_by_shared: bool = typer.Option(
+            False, "-S", "--filter-by-shared", help="Enable filtering by shared"
+    ),
+    is_shared: bool = typer.Option(
+        False, "-s", "--shared", help="Is it shared?"
+    ),
+    unique_to_filtered_account: bool = typer.Option(
+        False, "-U", "--unique-to-filtered-account", help="Filter out identifiers that are unique to the filtered account"
+    ),
+    filter_by_account: bool = typer.Option(
+        False, "-A", "--filter-by-account", help="Enable filtering by account id"
+    ),
+    account_id: int = typer.Option(
+        -1, "-a", "--filter-by-account-id", help="The account id to match on",
+    ),
+    filter_by_format: bool = typer.Option(
+        False, "-F", "--filter-by-format", help="Enable filtering by format"
+    ),
+    format: str = typer.Option(
+        None, "-f", "--format", help="The format to match on",
+    ),
+
+
+) -> None:
+    """
+    This command produces a list of identifiers according to different criteria
+    """
+    identifiers = set()
+    all_formats = {}
+    with input_file.open("r") as input:
+        data = json.load(input)
+        for datum in data:
+            availability = datum.get("availabilityV2", None)
+            title_id = datum["id"]
+            if availability:
+                accounts = availability["accounts"]
+                for account in accounts:
+                    acc_id = account["id"]
+                    account_filter_matches = True if not filter_by_account or (filter_by_account and account_id ==acc_id) else False
+                    unique_to_filtered_account_matches = True if not unique_to_filtered_account or (unique_to_filtered_account and len(accounts) == 1) else False
+                    shared_filter_matches = True if not filter_by_shared or (filter_by_shared and account.get("shared", False) == is_shared) else False
+                    format_filter_matches = True
+                    formats =  [x["id"] for x in datum.get("formats", None)]
+                    formats_to_match = [] if not format else format.split(",")
+
+                    for f in formats:
+                        all_formats[f] = all_formats.get(f, 0) + 1
+
+                    if filter_by_format:
+
+                        matching_formats = [x for x in formats if x in formats_to_match]
+                        format_filter_matches = True if len(matching_formats) > 0 else False
+
+                    if account_filter_matches and unique_to_filtered_account_matches and shared_filter_matches and format_filter_matches:
+                        identifiers.add(title_id)
+
+    with output_file.open("w") as output:
+        sorted_identifiers = sorted(identifiers)
+        for identifier in sorted_identifiers:
+            output.write(identifier + "\n")
+
+    print(f"There are {len(identifiers)} identifiers matching the criteria: "
+          f"shared={None if not filter_by_shared else is_shared}, "
+          f"format={None if not filter_by_format else format}, "
+          f"account_id={None if not filter_by_account else account_id},  "
+          f"unique_to_account={unique_to_filtered_account}.  The "
+          f"specific identifiers are can be found in the output file {output_file} \n" 
+          f"available formats: {all_formats}")
+
+def main() -> None:
+    run_typer_app_as_main(app, prog_name="overdrive_feed_analyzer")
+
+
+if __name__ == "__main__":
+    main()
